b950957d54cb6b1a3d05d05295462e7473c402fe
DHINA-G <129938353+Nmdhina@users.noreply.github.com>
2025-09-17 08:44:22 +0530

    Add files via upload

---
+++ list/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'list/index.html', context)
+
+def delete(request, pk):
+    task = Task.objects.get(id = pk)
+    task.delete()
+    return redirect('/')
